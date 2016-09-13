9865919f5125e018da028e6372eb3d6d2c7717b7
jiakun <jiakun@yongche.com>
2016-09-13 18:33:51 +0800

    env changed

---
+++ init/SysLog.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/9/5 0005
+ * Time: 18:01
+ */
+class SysLog
+{
+    public function errorLog(){
+        //error_log();
+    }
+
+}
---
+++ util/DateFormat.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/9/5 0005
+ * Time: 18:05
+ */
+class DateFormat
+{
+    public static function nowTime(){
+        echo time();
+    }
+}
