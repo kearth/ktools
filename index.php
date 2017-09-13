b9a1a5e7a175770d619de791603f4548e75a2f15
Kearth <myselfjk@sina.cn>
2017-09-13 18:06:03 +0800

    20170913

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-include("./php/test/index.php");
+
+use Php\Test\Test;
+
+define("ROOT_DIR", __DIR__);
+spl_autoload_register("autoload");
+function autoload($class)
+{
+    include(ROOT_DIR . "/" . $class . ".php");
+}
+
 
 Test::run();
 
