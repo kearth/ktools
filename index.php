fdc5cb779de00c5aea1a380115e0881c98ffec77
Kearth <myselfjk@sina.cn>
2017-09-15 18:01:03 +0800

    20170915

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-
 use Php\Test\Test;
 
 define("ROOT_DIR", __DIR__);
+
 spl_autoload_register("autoload");
+
 function autoload($class)
 {
+    $class = str_replace("\\",DIRECTORY_SEPARATOR, $class);
     include(ROOT_DIR . "/" . $class . ".php");
 }
 
