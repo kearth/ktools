a873080cabed272a15211bfd5832b7bf2522736a
Kearth <kearth@yeah.net>
2018-04-12 16:33:05 +0800

    20180412

--- index.php
+++ index.php
@@ -9,8 +9,22 @@ define("ROOT_DIR", __DIR__);
 //引入自动加载
 require(ROOT_DIR . '/vendor/autoload.php');
 
-require(ROOT_DIR . '/config/config.php');
+require(ROOT_DIR . '/tests/DongMeng.php');
 
 
+function run() 
+{
+    $arr = get_defined_functions();
+    $testMethods = $arr["user"];
+    
+    array_walk($testMethods, function($value){
+        if (strpos($value, "test") === 0) {
+            $res = call_user_func($value);
+            var_export($res);
+        }   
+    });
+}
 
+//运行
+run();
 
--- kearth/ceramic/src/HttpClient.php
+++ kearth/ceramic/src/HttpClient.php
@@ -4,33 +4,37 @@ namespace Ceramic;
 
 class HttpClient
 {
-    private $url = "";
+    private $url;
 
-    private $header = "text/plain";
+    private $ch;
 
-    private $params = []; 
+    private $version;
 
-    public function __construct()
+    private $params; 
+
+    public function __construct(string $url, array $params, $header = [])
     {
-    
+        if (empty($url)) {
+            throw new \Exception("url不能为空");
+        }
+
+        $this->params = $params;
+        $this->header = $header;
+
+        $this->ch = curl_init();
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
     }
 
-    public function post($url, $params)
+    /*
+     * [
+     *   "Content-type:text/plain"
+     *   "Content-length:100"
+     * ]
+     */
+    public function setHeader(array $header)
     {
-        $params = json_encode($params);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            "Content-Type: application/json; charset=utf-8",
-            "Content-Length:" . strlen($params)
-        ]);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-        $res = curl_exec($ch);
-        $errorno = curl_errno($ch);
-        curl_close($ch); 
-        return $res;
+        $this->header = $header;
     }
 
     public function get($url, $params)
@@ -44,5 +48,58 @@ class HttpClient
         $errorno = curl_errno($ch);
         curl_close($ch); 
         return $res;
+    }
+
+    public function post($url, $params)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        $res = curl_exec($ch);
+        $errorno = curl_errno($ch);
+        curl_close($ch); 
+        return $res;
+    }
+
+
+    public function setopt(array $setopt)
+    {
+        curl_setopt_array($this->ch, $setopt);
+        return $this;
+    }
+
+    public function escape($str)
+    {
+        curl_escape($this->ch, $str);
+        return $this;
+    }
+
+    public function close()
+    {
+        curl_close($this->ch);
+        return $this;
+    }
+
+    public function exec()
+    {
+        curl_exec($this->ch);
+        return $this;
+    }
+
+    public function reset()
+    {
+        curl_reset($this->ch);
+        return $this;
+    }
+
+    public function version($show = false)
+    {
+        $this->version = curl_version();
+        if ($show) {
+            var_export($this->version);
+        }
+        return $this;       
     }
 }
